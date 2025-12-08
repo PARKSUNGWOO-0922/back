3f1bfd3c17fdd4359f5de7a5590e648218e678c8
PARKSUNGWOO-0922 <mailcrybaby@gmail.com>
2025-12-08 14:59:25 +0900

    시작하기

--- thisisjava/src/ch01/sec09/Hello.java
+++ thisisjava/src/ch01/sec09/Hello.java
@@ -1,7 +1,12 @@
 package ch01.sec09;
+/**
+ * 문서화 주석
+ */
 
 public class Hello {
+	/*여러줄 주석 */
 	public static void main(String[] args) {
+		//ctrl + /:주석
 		System.out.println("Hello, Java");
 	}
 }
