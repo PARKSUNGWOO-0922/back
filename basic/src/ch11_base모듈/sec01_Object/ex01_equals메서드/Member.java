786c879434a57fe61057155cea6d18ee755e1333
PARKSUNGWOO-0922 <mailcrybaby@gmail.com>
2026-01-06 14:14:15 +0900

    base모듈0106

---
+++ basic/src/ch11_base모듈/sec01_Object/ex01_equals메서드/Member.java
@@ -0,0 +1,21 @@
+package ch11_base모듈.sec01_Object.ex01_equals메서드;
+
+public class Member {
+	public String id;
+
+	public Member(String id) {
+		this.id = id;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		//if(obj  instanceof Member)
+		//Member target = (Member) obj
+		if(obj instanceof Member target) {
+			if(id.equals(target.id)) {
+				return true;
+			}
+		}
+		return false;
+	}
+}
