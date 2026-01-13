489e18accbf83683d19399b49a3f45613bce785e
PARKSUNGWOO-0922 <mailcrybaby@gmail.com>
2026-01-13 17:56:28 +0900

    수정

--- basic/src/ch08_인터페이스/ex02_상수필드/RemoteControl.java
+++ basic/src/ch08_인터페이스/ex02_상수필드/RemoteControl.java
@@ -2,6 +2,7 @@ package ch08_인터페이스.ex02_상수필드;
 /**
  * 상수 필드
  * 	- [public static final] 타입 상수명 = 값;
+ * 	- [public static final] 생략가능, 인터페이스 변수는 무조건 상수	
  * 	- 모두 대문자 + 스네이크표기법(관례)
  */
 public interface RemoteControl {
--- basic/src/ch08_인터페이스/ex09_타입변환/CastingExample.java
+++ basic/src/ch08_인터페이스/ex09_타입변환/CastingExample.java
@@ -3,13 +3,16 @@ package ch08_인터페이스.ex09_타입변환;
 public class CastingExample {
 	public static void main(String[] args) {
 		//인터페이스 변수 선언과 구현 객체 대입
+		// 자동 타입 변환
 		Vehicle vehicle = new Bus();
 
 		//인터페이스를 통해서 호출
 		vehicle.run();
-		//vehicle.checkFare(); (x)
+		//vehicle.checkFare(); (x)// ❌ 인터페이스에는 없으므로 호출 불가
 
 		//강제 타입 변환후 호출
+		// Type mismatch: cannot convert from Vehicle to Bus
+		// Bus bus = vehicle; 
 		Bus bus = (Bus) vehicle; 
 		bus.run();
 		bus.checkFare();
