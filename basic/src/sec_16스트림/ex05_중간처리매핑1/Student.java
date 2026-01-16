cae3cbe2d9db6f066e86d98cee3807313d0a2d7e
PARKSUNGWOO-0922 <mailcrybaby@gmail.com>
2026-01-16 14:21:19 +0900

    배열_숫자범위_파일리스트_리소스_스트림얻기

---
+++ basic/src/sec_16스트림/ex05_중간처리매핑1/Student.java
@@ -0,0 +1,14 @@
+package sec_16스트림.ex05_중간처리매핑1;
+
+public class Student {
+	private String name;
+	private int score;
+
+	public Student(String name, int score) {
+		this.name = name;
+		this.score = score;
+	}
+
+	public String getName() { return name; }
+	public int getScore() { return score; }
+}
