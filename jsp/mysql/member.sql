8561f65f955c04d96e664b8465751316e3b3c049
PARKSUNGWOO-0922 <mailcrybaby@gmail.com>
2026-02-23 18:05:49 +0900

    jsp

--- jsp/mysql/member.sql
+++ jsp/mysql/member.sql
@@ -1,2 +1,20 @@
 -- DB생성
-CREATE SCHEMA 'testdb' DEFAULT CHARACTER SET utf8mb4;
+
+CREATE TABLE users (
+  id VARCHAR(20) PRIMARY KEY,
+  userpw VARCHAR(20) NOT NULL
+);
+INSERT INTO `testdb`.`users` (`id`, `userpw`) 
+VALUES ('hong', '1234');
+
+INSERT INTO users (id, userpw) VALUES ('freshlover', 'subway123');
+INSERT INTO users (id, userpw) VALUES ('sandwichking', 'eatfresh1');
+INSERT INTO users (id, userpw) VALUES ('veggie_hero', 'green2024');
+INSERT INTO users (id, userpw) VALUES ('footlong99', 'sub9999');
+INSERT INTO users (id, userpw) VALUES ('cookie_addict', 'sweet12');
+
+SELECT * FROM testdb.users;
+DELETE FROM `testdb`.`users` WHERE (`id` = 'hong');
+SHOW DATABASES;
+SELECT * FROM testdb.users;
+
